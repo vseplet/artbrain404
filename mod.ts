095a0ee2ab6ed6f90f150b5a32ef936192b41da0
Vsevolod Pletnev <150618497+vseplet@users.noreply.github.com>
2024-11-03 21:50:54 +0500

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,6 +1,13 @@
+const asyncFunctionFromString = (path) => {
+  return new Function(`return async function(path) {
+    await import(path)
+  }`)(path);
+};
+
 const init = async (im, path) => {
   console.log(im);
-  await import(im.resolve(path));
+  // await import(im.resolve(path));
+  asyncFunctionFromString(path)
 };
 
 export default {
