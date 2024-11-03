871f83202421fe4d0e31957da46499bab4d4ede2
Vsevolod Pletnev <150618497+vseplet@users.noreply.github.com>
2024-11-03 20:48:22 +0500

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,11 @@
-console.log(import.meta);
-import.meta.url = "/Users/sevapp";
-console.log(import.meta);
-console.log(Deno.cwd());
-await import("./config.ts")
+const init = () => {
+  console.log(import.meta);
+  import.meta.url = "/Users/sevapp";
+  console.log(import.meta);
+  console.log(Deno.cwd());
+  await import("./config.ts")
+};
+
+export default {
+  init
+}
