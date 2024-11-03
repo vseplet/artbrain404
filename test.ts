6a4a127a65f156195f95ebea9227543c1c756ceb
Vsevolod Pletnev <150618497+vseplet@users.noreply.github.com>
2024-11-03 20:54:10 +0500

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,4 +1,6 @@
-const init = async () => {
+const init = async (im) => {
+  console.log(im);
+  await import(im.resolve('./cfg.ts'));
   console.log(import.meta);
   import.meta.url = "/Users/sevapp";
   console.log(import.meta);
